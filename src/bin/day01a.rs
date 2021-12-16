f5258f4cb6635143f0d2a34d793f0580c839b069
arreme <a.reme@hotmail.com>
2021-12-16 17:07:27 +0100

    Advent of code day 1

--- src/bin/day01a.rs
+++ src/bin/day01a.rs
@@ -1,3 +1,25 @@
-fn main() {
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 
+fn main() {
+    let mut increased:u16 = 0;
+    if let Ok(lines) = read_lines("inputs/day01input.txt"){
+        let mut lines_iter = lines.into_iter();
+        let mut last_number = lines_iter.next().unwrap().unwrap().parse::<u16>().unwrap();
+        for line in lines_iter {
+            let current = line.unwrap().parse::<u16>().unwrap();
+            if current > last_number {
+                increased += 1;
+            }
+            last_number = current;
+        }
+    }
+    println!("Total of increased numbers: {}",increased);
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path> , {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
--- src/bin/day01b.rs
+++ src/bin/day01b.rs
@@ -1,3 +1,31 @@
+use std::collections::VecDeque;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 fn main() {
-    
+    let mut increased:u16 = 0;
+    if let Ok(lines) = read_lines("inputs/day01input.txt"){
+        let mut lines_iter = lines.into_iter();
+        let mut vec_deq:VecDeque<u16> = VecDeque::with_capacity(3);
+        vec_deq.push_back(lines_iter.next().unwrap().unwrap().parse::<u16>().unwrap());
+        vec_deq.push_back(lines_iter.next().unwrap().unwrap().parse::<u16>().unwrap());
+        vec_deq.push_back(lines_iter.next().unwrap().unwrap().parse::<u16>().unwrap());
+        let mut last_sum:u16 = vec_deq.iter().sum();
+        for line in lines_iter {
+            vec_deq.pop_front();
+            vec_deq.push_back(line.unwrap().parse::<u16>().unwrap());
+            let current:u16 = vec_deq.iter().sum();
+            if current > last_sum {
+                increased += 1;
+            }
+            last_sum = current;
+        }
+    }
+    println!("Total of increased numbers: {}",increased);
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+    where P: AsRef<Path> , {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
